98bc8e8ff6625dbf38420db44259f684454d7615
siddhuagre <118908130+siddhuagre@users.noreply.github.com>
2022-12-06 20:52:00 +0530

    Create addition of two no_from user value.c

---
+++ addition of two no_from user value.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int no1,no2,addition;
+    clrscr();
+    printf("\n enter the no1:");
+    scanf("%d",&no1);
+    printf("\n enter the no2:");
+    scanf("%d"&no2);
+    addition=no1+no2;
+  {
+      printf("\n addition=%d",addition);
+  }
+getch();
+}
