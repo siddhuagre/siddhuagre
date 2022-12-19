b67851726540c692e7afadc7d749ab14828af280
siddhuagre <118908130+siddhuagre@users.noreply.github.com>
2022-12-18 20:58:04 -0800

    using while loop_odd no's

---
+++ ending positon .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int e,i=1;
+  clrscr();
+  printf("\n Enter ending position:");
+  scanf("%d",&e);
+  while(i<=e)
+  {
+    printf("\n %d",i);
+    i=i+2;
+  }
+getch();
+}
