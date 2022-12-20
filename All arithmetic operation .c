5f27840a906bba587b79d035d78451a9f53b7aa9
siddhuagre <118908130+siddhuagre@users.noreply.github.com>
2022-12-20 14:10:57 +0530

    using two numbers

---
+++ All arithmetic operation .c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a,b,c,d,e;
+  float f;
+  clrscr();
+  printf(" Enter Two numbers:);
+  scanf("%d%d",&a,&b);
+  c=a+b;
+  d=a-b;
+  e=a*b;
+  f=a/b;
+  printf("\n addition=%d",c);
+  printf("\n substraction=%d",d);
+  printf("\n multiplication=%d",e);
+  printf("\n division=%f",f);
+ getch();
+         }
