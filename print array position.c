b21d308f2c3023ad49f6f123ce9a4b386017bc85
siddhuagre <118908130+siddhuagre@users.noreply.github.com>
2022-12-29 13:27:29 +0530

    using for loop

---
+++ print array position.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a[5]={10,20,30,40,50},i;
+  clrscr();
+  for(i=0;i<5;i++)
+  {
+    printf("\n Element in [%d] Position=%d",i,a[i]);
+  }
+getch();
+}
